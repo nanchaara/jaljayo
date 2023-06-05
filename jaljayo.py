41068de8ff305f8583746855b1599e19a7091ee3
nanchaara <azmiandi06@gmail.com>
2023-06-05 20:27:54 +0700

    initial upload

---
+++ jaljayo.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+st.title('Jaljayo')
+st.subheader('Diagnosis Awal Gangguan Mimpi Buruk')
+st.text('Dengan Nightmare Disorder Index (NDI)')
+
+with st.expander("Deskripsi"):
+    st.write("""
+            Nightmare Disorder Index (NDI) adalah sebuah alat skrining untuk penyakit Gangguan Mimpi Buruk.
+            Ada 3 kategori hasil dari alat skrining ini:
+            1. Tidak Memiliki Gangguan Mimpi Buruk
+            2. Gejala Gangguan Mimpi Buruk Sebagian/Subthreshold
+            3. Kemungkinan Gangguan Mimpi Buruk
+            """)
+    st.markdown('##')
+    st.warning("""Harap dicatat bahwa NDI dimaksudkan sebagai alat skrining
+                dan **TIDAK BOLEH** digunakan untuk menetapkan diagnosis definitif.
+                Silakan melakukan wawancara klinis dengan ahli yang terkait."""
+    )
+
+
+
+if 'pilihan' not in st.session_state:
+    st.session_state.pilihan = '0'
+
+
+def up_pil():
+    st.session_state.pilihan = st.session_state.pil1
+
+
+with st.form("NDI"):
+    q1 = st.radio("Berapa malam dalam seminggu Anda mengalami mimpi buruk (yang mengganggu, berkepanjangan, membekas dalam ingatan)?",
+                ('0 malam per minggu', '<1 malam per minggu', 
+                    '1-3 malam per minggu', '4-6 malam per minggu',
+                    '7 malam per minggu'), key='pil1')
+    
+    kirim = st.form_submit_button(label="Kirim", on_click=up_pil)
+
+
+if kirim:
+    if q1 == '0 malam per minggu':
+        st.subheader("Anda Tidak Memiliki Gangguan Mimpi Buruk.")
+    else:
+         switch_page("Jaljayo2")
---
+++ pages/jaljayo2.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+import pyautogui
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+st.title('Jaljayo')
+st.subheader('Diagnosis Awal Gangguan Mimpi Buruk')
+st.write('Dengan Nightmare Disorder Index (NDI)')
+
+st.markdown('##')
+        
+st.write('Halaman 2')
+
+with st.form("NDI2"):
+    q2 = st.radio("Seberapa sering Anda terbangun dari mimpi buruk DAN tersadar dengan cepat?",
+                ('Tidak pernah', 'Jarang', 'Kadang-kadang', 'Sering', 'Selalu'))
+
+    q3 = st.radio('Sejauh mana mimpi buruk mengganggu/meresahkan Anda secara umum?',
+                ('Tidak menggangu sama sekali', 'Sedikit mengganggu', 
+                'Agak mengganggu', 'Banyak mengganggu', 'Sangat mengganggu'))
+
+    q4 = st.radio("Sejauh mana mimpi buruk menyebabkan kesulitan dalam sosial, pekerjaan, atau bidang lain dalam hidup Anda?",
+                ('Tidak menggangu sama sekali', 'Sedikit mengganggu', 
+                'Agak mengganggu', 'Banyak mengganggu', 'Sangat mengganggu'))
+
+    q5 = st.radio("Sudah berapa lama Anda merasa terganggu oleh mimpi buruk?",
+                ('<1 minggu', '<1 bulan', 
+                '1-6 bulan', '6-12 bulan', '>12 bulan'))
+    
+    kirim = st.form_submit_button(label="Kirim")
+
+
+
+# edit ini
+if kirim:
+    if ((q2 == 'Tidak pernah' or q2 == 'Jarang') or
+        (q3 == 'Tidak menggangu sama sekali' or q3 == 'Sedikit mengganggu') or
+        (q4 == 'Tidak menggangu sama sekali') or (q4 == 'Sedikit mengganggu')):
+        st.subheader("Anda memiliki Gejala Gangguan Mimpi Buruk Sebagian/Subthreshold.")
+    else:
+        st.subheader('Anda memiliki Kemungkinan Gangguan Mimpi Buruk')
+
+        if st.session_state.pilihan == '<1 malam per minggu':
+            st.success('Dengan tingkat keparahan: Ringan')
+        elif st.session_state.pilihan == '7 malam per minggu':
+            st.error('Dengan tingkat keparahan: Parah')
+        else:
+            st.warning('Dengan tingkat keparahan: Sedang')
+
+
+        if q5 == '<1 minggu':
+            st.write('')        
+        elif q5 == '<1 bulan':
+            st.success('dan dengan tingkat keakutan: Akut')
+        elif q5 == '1-6 bulan':
+            st.warning('Dengan tingkat keakutan: Subakut')
+        else:
+            st.error('Dengan tingkat keakutan: Konstan')
+
+
+
+back = st.button('Kembali ke Halaman Utama')
+
+if back:
+    switch_page("jaljayo")
+    pyautogui.hotkey("ctrl","F5")
